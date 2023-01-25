696f9e23b68e6c11b0c2c59a49ecb025a8ecc2a9
Eduardo Lima <eduardolima2417@gmail.com>
2023-01-25 00:04:39 -0400

    commit

--- js/carrinho.js
+++ js/carrinho.js
@@ -7,7 +7,7 @@ if (document.readyState == "loading") {
 function ready() {
     const removeProductButtons = document.getElementsByClassName("remove-product-button")
     for (var i = 0; i < removeProductButtons.length; i++) {
-        removeProductButtons[i].addEventListener("click",removeProduct)
+        removeProductButtons[i].addEventListener("click", removeProduct)
     }
 
     const quantityInputs = document.getElementsByClassName("product-qtd-input")
@@ -50,40 +50,39 @@ function addToCartCart(event) {
     let newCartProduct = document.createElement("ul")
     newCartProduct.classList.add("cart-product")
 
-    newCartProduct.innerHTML = 
-    ` <li class="list-group-item py-3">
-    <div class="row g-3">
-        <div class="col-4 col-md-3 col-lg-2">
-            <a href="#">
-                <img src="${productImage}" class="img-thumbnail">
-            </a>
-        </div>
-        <div class="col-8 col-md-9 col-lg-7 col-xl-8 text-left align-self-center">
-            <h6>
-                <strong>${productTitle}</strong>
-            </h6>
-        </div>
-        <div class="text-start mt-2">
-            <small class="text-secondary cart-product-price">${productPrice}</small><br>
-        </div>
-        <div
-            class="col-6 offset-6 col-sm-6 offset-sm-6 col-md-4 offset-md-8 col-lg-3 offset-lg-0 col-xl-2 align-self-center mt-3">
-            <div class="input-group">
-
-                <input style="max-width: 50px;" class="form-control text-center border-dark rounded-3 product-qtd-input"
-                value="1" min="0" type="number">
-                </input>
-
-                <button
-                    class="btn btn-danger border-dark btn-sm ms-2 rounded-3 remove-product-button"
-                    type="button">
-                    Remover
-                </button>
+    newCartProduct.innerHTML =
+    `
+    <li class="list-group-item py-3">
+        <div class="row g-3">
+            <div class="col-4 col-md-4 col-lg-4">
+                <a href="#">
+                    <img src="${productImage}" class="img-thumbnail">
+                </a>
             </div>
+            <div class="col-8 col-md-9 col-lg-7 col-xl-8 text-left align-self-center">
+                <h6>
+                    <strong>${productTitle}</strong>
+                </h6>
+            </div>
+            <div class="text-start mt-2">
+                <small class="text-secondary cart-product-price">${productPrice}</small><br>
+            </div>
+            <div class="mt-3">
+                <div class="input-group">
+                    <input style="max-width: 50px;" class="form-control text-center border-dark rounded-3               product-qtd-input"
+                    value="1" min="0" type="number">
+                    </input>
+                    <button
+                        class="btn btn-danger border-dark btn-sm ms-2 rounded-3 remove-product-button"
+                        type="button">
+                        Remover
+                    </button>
+                    
             
+                </div>
+            </div>
         </div>
-    </div>
-</li>
+    </li>
     `
     const tableBody = document.querySelector(".carrinho tbody")
     tableBody.append(newCartProduct)
@@ -92,12 +91,13 @@ function addToCartCart(event) {
     newCartProduct.getElementsByClassName("remove-product-button")[0].addEventListener("click,", removeProduct)
 }
 
+// função que remove o produto
 function removeProduct(event) {
     event.target.parentElement.parentElement.parentElement.remove()
     updateTotal()
 }
 
-
+// função que atualiza o valor total
 function updateTotal() {
     let totalAmount = 0
     const cartProducts = document.getElementsByClassName("cart-product")
@@ -105,7 +105,7 @@ function updateTotal() {
         const productPrice = cartProducts[i].getElementsByClassName("cart-product-price")[0].innerText.replace("R$", "").replace(",", ".")
         const productQuantity = cartProducts[i].getElementsByClassName("product-qtd-input")[0].value
         totalAmount += productPrice * productQuantity
-}
+    }
 
     totalAmount = totalAmount.toFixed(2)
     totalAmount = totalAmount.replace(".", ",")
